6e6a099c6f26a6baa427c1dfd1317c38d038b3f4
Calimir <77618197+Calimir@users.noreply.github.com>
2021-01-18 20:14:10 +0100

    Changed Songlistrow from state to prop

--- src/components/Songlistrow.js
+++ src/components/Songlistrow.js
@@ -2,21 +2,13 @@ import React from 'react';
 
 class Songlistrow extends React.Component {
 
-	constructor(props) {
-		super();
-
-		this.state = {
-			song: props.song
-		};
-	}
-
 	render() {
 		return (
 			<tr>
-				<td>{this.state.song.title}</td>
-				<td>{this.state.song.artist}</td>
-				<td>{this.state.song.genre}</td>
-				<td>{this.state.song.rating}</td>
+				<td>{this.props.song.title}</td>
+				<td>{this.props.song.artist}</td>
+				<td>{this.props.song.genre}</td>
+				<td>{this.props.song.rating}</td>
 				<td></td>
 			</tr>
 		);
