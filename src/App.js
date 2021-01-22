fdac75ca33f45c204129dd7e02f0c6a4733297e2
Calimir <77618197+Calimir@users.noreply.github.com>
2021-01-22 14:27:15 +0100

    Cleanup of navigation CSS and added app CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from "react";
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './CSS/App.css';
+import Homepage from "./pages/Homepage";
 import Navigation from "./pages/Navigation";
+import Songpage from "./pages/Songpage";
+import Victorychartpage from "./pages/Victorychartpage";
 
 function App() {
     return (
-        <Navigation />
+        <Router>
+            <Navigation />
+            <div className="contentWrapper">
+                <Switch>
+                    <Route path="/songlist">
+                        <Songpage />
+                    </Route>
+                    <Route path="/victorychart">
+                        <Victorychartpage />
+                    </Route>
+                    <Route path="/">
+                        <Homepage />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
--- src/pages/Navigation.js
+++ src/pages/Navigation.js
@@ -1,38 +1,22 @@
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../CSS/navigation.css";
-import Homepage from "./Homepage";
-import Songpage from "./Songpage";
-import Victorychartpage from "./Victorychartpage";
 
 function Navigation() {
     return (
-        <Router>
-            <div className="wrapper">
-                <nav>
-                    <div className="navItem">
-                        <Link to="/">Home</Link>
-                    </div>
-                    <div className="navItem">
-                        <Link to="/songlist">Songlist</Link>
-                    </div>
-                    <div className="navItem">
-                        <Link to="/victorychart">Victory chart</Link>
-                    </div>
-                </nav>
-
-                <Switch>
-                    <Route path="/songlist">
-                        <Songpage />
-                    </Route>
-                    <Route path="/victorychart">
-                        <Victorychartpage />
-                    </Route>
-                    <Route path="/">
-                        <Homepage />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>);
+        <div className="navigationWrapper">
+            <nav>
+                <div className="navItem">
+                    <Link to="/">Home</Link>
+                </div>
+                <div className="navItem">
+                    <Link to="/songlist">Songlist</Link>
+                </div>
+                <div className="navItem">
+                    <Link to="/victorychart">Victory chart</Link>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default Navigation;
